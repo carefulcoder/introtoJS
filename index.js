13fd5e8fdc3b76ce4066ec06a40d6820bf99b8ee
carefulcoder <59884103+carefulcoder@users.noreply.github.com>
2020-04-29 17:25:27 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-// This is my first JS code
+ //My first JavaScript code
 console.log('Hello World');
+
+let name = 'Tess'; // string literal
+let age = 20; // number literal
+let isApproved = false; //Boolean literal
+let firstName = undefined;
+let selectedColor = null;
+
+console.log(name);
+
+let firstName = 'Tess';
+let lastName = 'Moran';
+
+const interestRate = 0.3
+interestRate = 1;
+console.log(interestRate);
+
+//performing a task
+function greet(name, lastName) { //parameter
+    console.log('Hello ' + name + ' ' + lasName)
+}
+
+greet('John', 'Smith'); //argument
+greet('Mary');
+
+//calculating a value
+function square(number) {
+    return number * number;
+}
+
+let number = square(2);
+console.log(number);
